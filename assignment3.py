c28e24d414d05f81ee393cf84cbd1f1934117973
GreysonGough <greygough@gmail.com>
2026-01-27 09:08:01 -0800

    updated

--- assignment3.py
+++ assignment3.py
@@ -7,7 +7,7 @@ print("This is the game of heads or tails")
 
 while True:
 
-    guess = input("Enter 'heads' or 'tails': ")
+    guess = input("Enter 'heads' or 'tails': ").lower()
 
     coin_flip = random.choice(["heads", "tails"])
 
@@ -17,15 +17,15 @@ while True:
         print("You are correct!")
         correct_g +=1
 
-    elif guess != coin_flip:
+    elif guess in ["heads", "tails"]:
         print("Incorrect!")
         incorrect_g +=1
     
     else:
         print("invalid input")
 
-    again = input("Would you like to play again? (Yes, No, case sensitive)")
+    again = input("Would you like to play again?").lower()
 
-    if again == "No":
+    if again == "no":
         print(f"Okay, you had {correct_g} correct guesses and {incorrect_g} incorrect guesses")
         break
